f970ebaaaa55d764f1910cf54e8b5896e1418765
vvenger <alexander_venger@mail.ru>
2025-01-01 23:41:28 +0300

    homework1: add migrations

--- migrations/20240331200532_users.up.sql
+++ migrations/20240331200532_users.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE users
 (
-    id uuid NOT NULL,
-    password VARCHAR(72) NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
+    id uuid     NOT NULL,
+    password    VARCHAR(72) NOT NULL,
+    first_name  VARCHAR(50) NOT NULL,
     second_name VARCHAR(50) NOT NULL,
-    birthdate TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    biography TEXT,
-    city VARCHAR(30) NOT NULL,
+    birthdate   TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    biography   TEXT,
+    city        VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
